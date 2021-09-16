5a812d86b384cd821821b6e67f456479e52974c5
vasanth.narayanan <vasanth.narayanan@aspiresys.com>
2021-09-16 12:10:10 +0530

    update images list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,31 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ImageUploadComponent } from './image-upload/image-upload.component';
 import { ImageListComponent } from './image-list/image-list.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+
 
 @NgModule({
   declarations: [AppComponent, ImageUploadComponent, ImageListComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [
+    BrowserModule, 
+    AppRoutingModule, 
+    HttpClientModule, 
+    BrowserAnimationsModule, 
+    MatSliderModule,
+    MatTableModule,
+    MatButtonModule,
+    MatCardModule
+  ],
+  exports: [
+    MatSliderModule,
+    MatTableModule,
+    MatButtonModule,
+    MatCardModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/image-list/image-list.component.ts
+++ src/app/image-list/image-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
 import { ImageUploadService } from '../image-upload.service';
 
 @Component({
@@ -9,12 +10,29 @@ import { ImageUploadService } from '../image-upload.service';
 export class ImageListComponent implements OnInit {
 
   images: any
+  dataSource: any = []
+  displayedColumns: string[] = ['position', 'Key', 'modifieddate', 'size', 'action'];
 
+  
   constructor(private imageUploadService: ImageUploadService) {}
 
   ngOnInit() {
     this.imageUploadService.getImages().subscribe(res => {
       this.images = res
+      let incrementor = 1
+      this.dataSource = new MatTableDataSource();
+      this.images.forEach(element => {
+        this.dataSource.data.push({
+          'position': incrementor,
+          'key': element.Key,
+          'modifieddate':  element.LastModified,
+          'size': (element.Size / (1024*1024)).toFixed(2),
+          'url':  'https://vasi-aws-training.s3.amazonaws.com/'+element.Key
+        })
+
+        incrementor++
+      });
+      console.log(this.dataSource)
     });
   }
 
